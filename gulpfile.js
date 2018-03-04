a969fb25ca9152dd067cc2fb6a668f25bb226291
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2018-03-03 18:56:09 -0800

    remove gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -13,27 +13,5 @@ var knownOptions = {
 var options = minimist(process.argv.slice(2), knownOptions);
 
 gulp.task('default', function () {
-
-	var packagePaths = ['**', 
-					'!**/_package/**', 
-					'!**/typings/**',
-					'!typings', 
-					'!_package', 
-					'!gulpfile.js']
-	
-	//add exclusion patterns for all dev dependencies
-	var packageJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
-	var devDeps = packageJSON.devDependencies;
-
-	for(var propName in devDeps)
-	{
-		var excludePattern1 = "!**/node_modules/" + propName + "/**";
-		var excludePattern2 = "!**/node_modules/" + propName;
-		packagePaths.push(excludePattern1);
-		packagePaths.push(excludePattern2);
-	}
-	
-    return gulp.src(packagePaths)
-        .pipe(zip(options.packageName))
-        .pipe(gulp.dest(options.packagePath));
+    console.log("Nothing to see here!");
 });
