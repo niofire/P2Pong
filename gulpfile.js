44a35a4f4f39f6438dfaa0b04a5f7356546fbf1d
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2018-03-03 19:30:12 -0800

    re-add gulp zip

--- gulpfile.js
+++ gulpfile.js
@@ -13,5 +13,27 @@ var knownOptions = {
 var options = minimist(process.argv.slice(2), knownOptions);
 
 gulp.task('default', function () {
-    console.log("Nothing to see here!");
+
+	var packagePaths = ['**', 
+					'!**/_package/**', 
+					'!**/typings/**',
+					'!typings', 
+					'!_package', 
+					'!gulpfile.js']
+	
+	//add exclusion patterns for all dev dependencies
+	var packageJSON = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
+	var devDeps = packageJSON.devDependencies;
+
+	for(var propName in devDeps)
+	{
+		var excludePattern1 = "!**/node_modules/" + propName + "/**";
+		var excludePattern2 = "!**/node_modules/" + propName;
+		packagePaths.push(excludePattern1);
+		packagePaths.push(excludePattern2);
+	}
+	
+    return gulp.src(packagePaths)
+        .pipe(zip(options.packageName))
+        .pipe(gulp.dest(options.packagePath));
 });
