bb193fbdff656f002833128f846aa671f7fcab7f
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2018-03-10 12:51:59 -0800

    Prevent space + arrows to affect page navigation

--- wwwroot/scripts/Entities/InputManager.js
+++ wwwroot/scripts/Entities/InputManager.js
@@ -1,24 +1,28 @@
 
-var __inputManager = (function(){
+var __inputManager = (function () {
     var _keysDown = {};
 
-    window.addEventListener("keydown", function(event){
+    window.addEventListener("keydown", function (event) {
         _keysDown[event.keyCode || e.which] = true;
+        // Disable Space + Arrow keys
+        if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
 
-    },true);
-    
-    window.addEventListener("keyup", function(event){
+    }, true);
+
+    window.addEventListener("keyup", function (event) {
         _keysDown[event.keyCode || e.which] = false;
-    },true)
+    }, true)
 
-    window.addEventListener("blur",function(event){
+    window.addEventListener("blur", function (event) {
         _keysDown[83] = false;
         _keysDown[87] = false;
         _keysDown[38] = false;
         _keysDown[40] = false;
     });
 
-    return{
+    return {
         keysDown: _keysDown
     }
 })();
